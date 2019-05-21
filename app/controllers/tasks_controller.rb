d8e39dc53395ff5a10d0b04328d691a7ee92e75c
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-05-21 13:53:40 +0200

    creation page index et show avec léger css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,19 @@
 class TasksController < ApplicationController
     def index         # GET /tasks
       @tasks = Task.all
+      if @tasks.count == 0
+        @title_page = "Vous n'avez pas de tache"
+      elsif @tasks.count == 1
+        @title_page = "Voici votre tache"
+      else
+        @title_page = "Voici vos #{@tasks.count} taches"
+      end
     end
   
     def show          # GET /tasks/:id
       @task = Task.find(params[:id])
+      @check = @task.completed ? 'far fa-check-circle checked' : 'far fa-circle'
+      @text_check = @task.completed ? 'This task is completed' : 'This task is not completed'
     end
   
     def new           # GET /tasks/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get    "tasks",          to: "tasks#index"
 
-  get    "tasks/new",      to: "tasks#new"
+  get    "tasks/new",      to: "tasks#new", as: "task_new"
   post   "tasks",          to: "tasks#create"
-  
-  get    "tasks/:id",      to: "tasks#show"
+
+  get    "tasks/:id",      to: "tasks#show", as: "task"
 
   get    "tasks/:id/edit", to: "tasks#edit"
   patch  "tasks/:id",      to: "tasks#update"
